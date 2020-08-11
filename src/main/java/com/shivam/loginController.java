e25ba6035b41fc5a173911b589dbf97a60e8e9af
Shivam Aggarwal <shivam01101995@gmail.com>
2020-08-11 19:17:33 +0530

    Basic MVC Setup completed

--- src/main/java/com/shivam/loginController.java
+++ src/main/java/com/shivam/loginController.java
@@ -1,9 +1,15 @@
 package com.shivam;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+ @Controller
 public class loginController {
 	
-	void doLogin() {
+	@RequestMapping("/doLogin")
+	String doLogin() {
 		System.out.println("Trying to LOGIN!!!!");
+		return "loginSuccessPage.jsp";
 	}
 	
 }
